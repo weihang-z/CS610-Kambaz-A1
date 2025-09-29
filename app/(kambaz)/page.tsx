f6f3c016076957460bebb7a3a00f7cf6e5e86dec
weihang-z <weihangzeng79@gmail.com>
2025-09-29 17:25:31 -0400

    fix small problems

--- app/(kambaz)/page.tsx
+++ app/(kambaz)/page.tsx
@@ -1,16 +1,6 @@
 "use client";
 
-import Link from "next/link";
+import { redirect } from "next/navigation";
 export default function Kambaz() {
-  return (
-    <div>
-      <h2>Kambaz Home Page</h2>
-      <br />
-      <Link href="/Account/Signup">Sign up</Link>
-      <br />
-      <Link href="/Account/Signin">Sign in</Link>
-      <br />
-      <Link href="/Labs/Lab1">Lab Exercises Landing Page</Link>
-    </div>
-  );
+  redirect("/Account/Signup");
 }
